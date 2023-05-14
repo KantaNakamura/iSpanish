535a04f99f6d9cb097fd82f72a995bb0b9de9317
KantaNakamura <0706363@gmail.com>
2023-05-14 13:48:20 +0800

    create tutor detail view

--- iSpanish/search_tutors/urls.py
+++ iSpanish/search_tutors/urls.py
@@ -5,4 +5,5 @@ app_name = 'search_tutors'
 
 urlpatterns = [
     path('tutors-list/', views.TutorsListView.as_view(), name='tutors-list'),
+    path('tutors-detail/<int:pk>', views.TutorsDetailView.as_view(), name='tutors-detail'),
 ]
--- iSpanish/search_tutors/views.py
+++ iSpanish/search_tutors/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import View
+from django.views.generic.detail import DetailView
 from django.shortcuts import render
 
 
@@ -8,7 +9,16 @@ from accounts.models import Users
 
 class TutorsListView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-            tutors = Users.objects.filter(is_tutor=True).all()
-            return render(request, 'searchTutors/tutors_list.html', {
-                'tutors': tutors,
-                })
+        tutors = Users.objects.filter(is_tutor=True).all()
+        return render(request, 'searchTutors/tutors_list.html', {
+            'tutors': tutors,
+            })
+        
+        
+class TutorsDetailView(LoginRequiredMixin, DetailView):
+    model = Users
+    template_name = 'searchTutors/tutors_detail.html'
+    
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
