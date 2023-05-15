56c2847d66a7ed74e3ebe2d3db43fca34f95b36f
KantaNakamura <0706363@gmail.com>
2023-05-15 12:11:42 +0800

    create update profile func

--- iSpanish/search_tutors/urls.py
+++ iSpanish/search_tutors/urls.py
@@ -6,4 +6,5 @@ app_name = 'search_tutors'
 urlpatterns = [
     path('tutors-list/', views.TutorsListView.as_view(), name='tutors-list'),
     path('tutors-detail/<int:pk>', views.TutorsDetailView.as_view(), name='tutors-detail'),
+    path('update-profile/<slug:username>', views.UpdateProfileView.as_view(), name='update-profile'),
 ]
--- iSpanish/search_tutors/views.py
+++ iSpanish/search_tutors/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import View
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 
 from accounts.models import Users
+from .forms import UpdateProfileForm
 
 
 class TutorsListView(LoginRequiredMixin, View):
@@ -22,3 +25,16 @@ class TutorsDetailView(LoginRequiredMixin, DetailView):
     def get_context_data(self,**kwargs):
         context = super().get_context_data(**kwargs)
         return context
+    
+    
+class UpdateProfileView(LoginRequiredMixin, UpdateView):
+    template_name = 'searchTutors/update_profile.html'
+    form_class = UpdateProfileForm
+    model = Users
+    slug_field = 'username'
+    slug_url_kwarg = 'username'
+    
+    def get_success_url(self):
+        request_user =  Users.objects.get(username=self.object.username)
+        return reverse_lazy('search_tutors:tutors-detail', kwargs={'pk': request_user.id})
+    
